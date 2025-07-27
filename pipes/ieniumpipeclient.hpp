eb3d4f41ae2d5ea07f38d6750258685dc3c6330a
Hendrik Jonas <hendrik.jonas01@gmail.com>
2025-07-27 13:13:19 +0200

    Added pipes library

---
+++ pipes/ieniumpipeclient.hpp
@@ -0,0 +1,101 @@
+#pragma once
+
+
+#include <windows.h>
+#include <iostream>
+#include <string>
+
+
+
+class IeniumPipeClient
+{
+    private:
+    HANDLE hPipe;
+    std::string pipeName;
+    bool connected;
+
+    public:
+    IeniumPipeClient (const std::string& name) : pipeName ("\\\\.\\pipe\\" + name) {}
+    virtual ~IeniumPipeClient ()
+    {
+        Disconnect ();
+    }
+
+    bool Connect (int timeout_ms = 5000)
+    {
+        hPipe = CreateFile (
+            pipeName.c_str (),
+            GENERIC_WRITE,
+            0,
+            NULL,
+            OPEN_EXISTING,
+            0,
+            NULL
+        );
+
+        if (hPipe != INVALID_HANDLE_VALUE)
+        {
+            connected = true;
+            return true;
+        }
+
+        DWORD error = GetLastError ();
+        if (error == ERROR_PIPE_BUSY)
+        {
+            if (WaitNamedPipe (pipeName.c_str (), timeout_ms))
+            {
+                return Connect(0);
+            }
+        }
+
+        std::cerr << "Failed to connect to pipe: " << pipeName << " Error: " << error << std::endl;
+        return false;
+    }
+
+    void Disconnect ()
+    {
+        if (hPipe != INVALID_HANDLE_VALUE)
+        {
+            CloseHandle (hPipe);
+            hPipe = INVALID_HANDLE_VALUE;
+        }
+
+        connected = false;
+    }
+
+    bool SendData (const std::string& message)
+    {
+        if (!connected || hPipe == INVALID_HANDLE_VALUE)
+        {
+            connected = false;
+            std::cerr << "Not connected to pipe." << std::endl;
+            return false;
+        }
+
+        DWORD bytes_written;
+        bool sucess = WriteFile (
+            hPipe,
+            message.c_str (),
+            static_cast<DWORD>(message.length ()),
+            &bytes_written,
+            NULL
+        );
+
+        if (!sucess)
+        {
+            DWORD error = GetLastError ();
+            std::cerr << "Failed to write to pipe. Error: " << error << std::endl;
+
+            if (error == ERROR_BROKEN_PIPE || ERROR_NO_DATA)
+            {
+                connected = false;
+                CloseHandle (hPipe);
+                hPipe = INVALID_HANDLE_VALUE;
+            }
+            return false;
+        }
+
+        FlushFileBuffers (hPipe);
+        return true;
+    }
+};
---
+++ pipes/ieniumpipeserver.hpp
@@ -0,0 +1,197 @@
+#pragma once
+
+
+#include <atomic>
+#include <functional>
+#include <iostream>
+#include <vector>
+#include <windows.h>
+#include <string>
+
+class IeniumPipeServer
+{
+    private:
+    HANDLE hPipe;
+    std::string pipeName;
+    const int pipeType = PIPE_TYPE_MESSAGE;     // String data is sent
+    const int pipeMode = PIPE_WAIT;             // BLocking mode
+    const DWORD bufferSize;
+    const DWORD MAX_BUFFER_SIZE = 1024 * 1024;  // 1MB
+
+    std::atomic<bool> running;
+    std::function<void (std::string)> processMessageCallback;
+    std::function<void (bool)> connectionStatusChangedCallback;
+
+    public:
+    IeniumPipeServer (const std::string& name, const DWORD buffer_size) : pipeName ("\\\\.\\pipe\\" + name), bufferSize (buffer_size) {}
+    
+    bool StartServer ()
+    {
+        hPipe = CreateNamedPipe (
+            pipeName.c_str (),
+            PIPE_ACCESS_INBOUND,
+            PIPE_TYPE_MESSAGE,
+            PIPE_READMODE_MESSAGE,
+            PIPE_UNLIMITED_INSTANCES,
+            bufferSize,
+            bufferSize,
+            0 
+        );
+
+        if (hPipe == INVALID_HANDLE_VALUE)
+        {
+            std::cerr << "Failed to create named Pipe. Error: " << GetLastError () << std::endl;
+            return false;
+        }
+
+        std::cout << "Named pipe server created: " << pipeName << std::endl;
+        running.store (true);
+        return true;
+    }
+
+    void Shutdown ()
+    {
+        running.store (false);
+        if (hPipe != INVALID_HANDLE_VALUE)
+        {
+            CloseHandle (hPipe);
+        }
+    }
+
+    void SetProcessMessageCallback (std::function<void (std::string)> cb)
+    {
+        processMessageCallback = cb;
+    }
+
+    void SetConnectionStatusChangedCallBack (std::function<void (bool)> cb)
+    {
+        connectionStatusChangedCallback = cb;
+    }
+
+    void ListenForMessages ()
+    {
+        while (running)
+        {
+            std::cout << "Waiting for client connection" << std::endl;
+
+            bool connected = ConnectNamedPipe (hPipe, NULL) ? TRUE : (GetLastError () == ERROR_PIPE_CONNECTED);
+
+            if (connected)
+            {
+                std::cout << "Client connected" << std::endl;
+                if (connectionStatusChangedCallback != NULL)
+                    connectionStatusChangedCallback (true);
+
+                while (running)
+                {
+                    std::string full_message = ReadFullMessage ();
+
+                    if (!full_message.empty ())
+                    {
+                        if (processMessageCallback != NULL)
+                            processMessageCallback (full_message);
+                        else
+                            std::cout << "ProcessMessageCallback function not set. Message is not being processed!" << std::endl;
+                    }
+                    else
+                    {
+                        DWORD error = GetLastError ();
+                        if (error == ERROR_BROKEN_PIPE)
+                        {
+                            std::cout << "Client disconnected." << std::endl;
+                            break;
+                        }
+                        else if (error != ERROR_SUCCESS)
+                        {
+                            std::cerr << "Read failed. Error: " << error << std::endl;
+                            break;
+                        }
+                    }
+                }
+                DisconnectNamedPipe (hPipe);
+                if (connectionStatusChangedCallback != NULL)
+                    connectionStatusChangedCallback (false);
+            }
+            else
+            {
+                std::cerr << "ConnecNamedPipeFailed. Error: " << GetLastError () << std::endl;
+                break;
+            }
+        }
+    }
+
+    private:
+    std::string ReadFullMessage ()
+    {
+        std::vector<char> buffer (bufferSize);
+        std::string full_message;
+        DWORD bytes_read;
+        DWORD total_bytes_avail;
+        DWORD bytes_left_this_message;
+
+        bool peek_success = PeekNamedPipe (
+            hPipe,
+            NULL,
+            0,
+            NULL,
+            &total_bytes_avail,
+            &bytes_left_this_message
+        );
+
+        if (!peek_success)
+        {
+            return "";
+        }
+        SetLastError(0);
+
+        if (total_bytes_avail == 0)
+        {
+            std::cout << "Message is empty" << std::endl;
+            return "";
+        }
+
+        if (bytes_left_this_message > buffer.size ())
+        {
+            if (bytes_left_this_message > MAX_BUFFER_SIZE)
+            {
+                std::cerr << "Message too large: " << bytes_left_this_message << " bytes. Discarding message" << std::endl;
+                std::vector<char> discardBuffer (bufferSize);
+                DWORD total_discarded = 0;
+
+                while (total_discarded < bytes_left_this_message)
+                {
+                    DWORD to_read = (discardBuffer.size () < (bytes_left_this_message - total_discarded)) ? discardBuffer.size () : (bytes_left_this_message - total_discarded); 
+
+                    bool success = ReadFile (
+                        hPipe,
+                        discardBuffer.data (),
+                        to_read,
+                        &bytes_read,
+                        NULL
+                    );
+                    if (!success || bytes_read == 0)
+                    {
+                        break;
+                    }
+                    total_discarded += bytes_read;
+                }
+                return "";
+            }
+            buffer.resize (bytes_left_this_message);
+        }
+
+        bool success = ReadFile (
+            hPipe,
+            buffer.data (),
+            static_cast<DWORD>(buffer.size ()),
+            &bytes_read,
+            NULL
+        );
+
+        if (success && bytes_read > 0)
+        {
+            return std::string (buffer.data (), bytes_read);
+        }
+        return "";
+    }
+};
